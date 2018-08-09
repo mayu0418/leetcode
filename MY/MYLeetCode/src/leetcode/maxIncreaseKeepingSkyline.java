7e8c560cd96e1854979cc72842ed67caac8652dd
mayu <mayu@lianjia.com>
2018-08-09 23:06:09 +0800

    [add]天际线

---
+++ MY/MYLeetCode/src/leetcode/maxIncreaseKeepingSkyline.java
@@ -0,0 +1,60 @@
+package leetcode;
+
+public class maxIncreaseKeepingSkyline {
+	/*在二维数组grid中，grid[i][j]代表位于某处的建筑物的高度。 
+	我们被允许增加任何数量（不同建筑物的数量可能不同）的建筑物的高度。 
+	高度 0 也被认为是建筑物。
+	最后，从新数组的所有四个方向（即顶部，底部，左侧和右侧）观看的“天际线”必须与原始数组的天际线相同。 
+	城市的天际线是从远处观看时，由所有建筑物形成的矩形的外部轮廓。 请看下面的例子。
+	建筑物高度可以增加的最大总和是多少？
+	例子：
+	输入： grid = [[3,0,8,4],[2,4,5,7],[9,2,6,3],[0,3,1,0]]
+	输出： 35
+	解释： 
+	The grid is:
+	[ [3, 0, 8, 4], 
+	  [2, 4, 5, 7],
+	  [9, 2, 6, 3],
+	  [0, 3, 1, 0] ]
+	从数组竖直方向（即顶部，底部）看“天际线”是：[9, 4, 8, 7]
+	从水平水平方向（即左侧，右侧）看“天际线”是：[8, 7, 9, 3]
+	在不影响天际线的情况下对建筑物进行增高后，新数组如下：
+	gridNew = [ [8, 4, 8, 7],
+	            [7, 4, 7, 7],
+	            [9, 4, 8, 7],
+	            [3, 3, 3, 3] ]*/
+	public int maxIncreaseKeepingSkyline(int[][] grid) {
+		if (grid.length == 0 || grid[0].length == 0) {
+			return 0;
+		}
+        int[] rowMaxAry = new int[grid.length];
+        int[] colMaxAry = new int[grid[0].length];
+        for (int i = 0; i < rowMaxAry.length; i++) {
+        	int max = 0;
+			for (int j = 0; j < colMaxAry.length; j++) {
+				max = Math.max(max, grid[i][j]);
+			}
+			rowMaxAry[i] = max; 
+		}
+        for (int i = 0; i < colMaxAry.length; i++) {
+        	int max = 0;
+			for (int j = 0; j < rowMaxAry.length; j++) {
+				max = Math.max(max, grid[j][i]);
+			}
+			colMaxAry[i] = max;
+		}
+        int count = 0;
+        for (int i = 0; i < rowMaxAry.length; i++) {
+			for (int j = 0; j < colMaxAry.length; j++) {
+				count += Math.min(rowMaxAry[i], colMaxAry[j]) - grid[i][j];
+			} 
+		}
+		return count;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
