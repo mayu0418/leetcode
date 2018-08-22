9164ccd02379d84d0a582982d5598b452188da27
mayu <mayu@lianjia.com>
2018-08-22 20:11:12 +0800

    861. Score After Flipping Matrix

---
+++ MY/MYLeetCode/src/leetcode/Score_After_Flipping_Matrix.java
@@ -0,0 +1,57 @@
+package leetcode;
+
+public class Score_After_Flipping_Matrix {
+
+	/*
+	有一个二维矩阵 A 其中每个元素的值为 0 或 1 。
+	移动是指选择任一行或列，并转换该行或列中的每一个值：将所有 0 都更改为 1，将所有 1 都更改为 0。
+	在做出任意次数的移动后，将该矩阵的每一行都按照二进制数来解释，矩阵的得分就是这些数字的总和。
+	返回尽可能高的分数。
+	示例：
+	输入：[[0,0,1,1],[1,0,1,0],[1,1,0,0]]
+	输出：39
+	解释：
+	转换为 [[1,1,1,1],[1,0,0,1],[1,1,1,1]]
+	0b1111 + 0b1001 + 0b1111 = 15 + 9 + 15 = 39
+	提示：
+	1 <= A.length <= 20
+	1 <= A[0].length <= 20
+	A[i][j] 是 0 或 1
+	*/
+	
+	public int matrixScore(int[][] A) {
+        for (int i = 0; i < A.length; i++) {
+			if (A[i][0] == 0) {
+				for (int j = 0; j < A[i].length; j++) {
+					A[i][j] = A[i][j] == 0 ? 1 : 0;
+				}
+			}
+		}
+        for (int i = 0; i < A[0].length; i++) {
+			int count = 0;
+			for (int j = 0; j < A.length; j++) {
+				count += A[j][i];
+			}
+			if (count <= A.length / 2) {
+				for (int j = 0; j < A.length; j++) {
+					A[j][i] = A[j][i] == 0 ? 1 : 0;
+				}
+			}
+		}
+        int count = 0;
+        for (int i = 0; i < A.length; i++) {
+			String string = "";
+			for (int j = 0; j < A[0].length; j++) {
+				string += A[i][j];
+			}
+			count += Integer.valueOf(string,2);
+		}
+        return count;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
