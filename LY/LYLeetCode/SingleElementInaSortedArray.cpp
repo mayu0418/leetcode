b9fd7438b298a60ef26ac94d8a91deb791fcd918
liyi02 <liyi02@lianjia.com>
2018-09-29 22:01:25 +0800

    改 有序数组中的单一元素 时间复杂度不对bug

--- LY/LYLeetCode/SingleElementInaSortedArray.cpp
+++ LY/LYLeetCode/SingleElementInaSortedArray.cpp
@@ -8,20 +8,26 @@
 
 #include <iostream>
 int singleNonDuplicate(int* nums, int numsSize) {
-    int index=0;
-    int result=-1;
-    while (index<numsSize-1) {
-        if (nums[index] == nums[index+1]) {
-            index+=2;
+    int start=0;
+    int end=numsSize-1;
+    while (start<end) {
+        int mid=(start+end)/2;
+        if (mid+1<numsSize&&nums[mid]==nums[mid+1]) {
+            if (mid%2==0) {
+                start+=2;
+            } else {
+                end-=1;
+            }
+        } else if(mid-1>=0&&nums[mid]==nums[mid-1]) {
+            if (mid%2==0) {
+                end-=1;
+            } else {
+                start+=2;
+            }
         } else {
-            result=nums[index];
-            break;
+            return nums[mid];
         }
-    }
-    if (index == numsSize-1) {
-        result=nums[numsSize-1];
-    }
-    return result;
+    } return nums[start];
 }
 
 int main(int argc, const char * argv[]) {
