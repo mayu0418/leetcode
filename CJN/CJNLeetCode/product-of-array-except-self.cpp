3ef9e8549da1a23d68898cc0bbe26a33cbea4ae6
cjnhust <303879653@qq.com>
2018-10-17 14:51:18 +0800

    product-of-array-except-self

---
+++ CJN/CJNLeetCode/product-of-array-except-self.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+     vector<int> left(nums.size());
+    vector<int> right(nums.size());
+    vector<int> result(nums.size());
+    left[0] = 1;
+    right[nums.size()-1] = 1;
+    for(int i=1;i<nums.size();i++){
+        left[i] = left[i-1]*nums[i-1];
+    }
+    for(int i= nums.size() - 2; i >= 0; i--){
+        right[i] = right[i+1]*nums[i+1];
+    }
+    for(int i = 0;i<nums.size();i++){
+        result[i] = left[i]*right[i];
+    }
+    return result;
+    }
+};
