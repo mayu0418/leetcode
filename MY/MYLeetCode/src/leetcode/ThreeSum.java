5f0a533707ca52edd542996ccfa40b73dc1c5919
mayu <mayu.0418@bytedance.com>
2019-04-24 22:56:05 +0800

    3sum

---
+++ MY/MYLeetCode/src/leetcode/ThreeSum.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums.length == 0) return new ArrayList<>();
+        Set resList = new HashSet();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    resList.add(Arrays.asList(nums[i],nums[j++],nums[k--]));
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return new ArrayList<>(resList);
+    }
+}
